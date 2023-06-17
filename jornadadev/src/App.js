f8ce8a63778805b8ef29dfca4bac15fc6052c476
Esther de Freitas <estherdefreitas@gmail.com>
2023-06-17 17:19:11 -0300

    Add sidebar

--- jornadadev/src/App.js
+++ jornadadev/src/App.js
@@ -6,9 +6,24 @@ function App() {
     <div className='App'>
       <h1>Tekoteko</h1>
         <div className='app_videos'>
-          <Video/>
-          <Video/>
-          <Video/>
+          <Video
+            likes={100}
+            messages={200}
+            shares={300}
+            name='Celso'
+            description='Goleiro de garra'
+            music='É GOOOOOOL'
+            url='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'
+          />
+          <Video
+            likes={10}
+            messages={300}
+            shares={500}
+            name='Brenda'
+            description='Brincando com pet'
+            music='Meauw'
+            url='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z'
+          />
         </div>
     </div>
   );
--- jornadadev/src/pages/componentes/footer/VideoFooter.js
+++ jornadadev/src/pages/componentes/footer/VideoFooter.js
@@ -2,16 +2,16 @@ import React from 'react'
 import "./videoFooter.css"
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 
-function VideoFooter() {
+function VideoFooter({name, description, music}) {
 	return (
 		<div className="videoFooter">
 			<div className="videoFooter_text">
-				<h3>@esther EBAC</h3>
-				<p>Gatinho fofinho</p>
+				<h3>@{name}</h3>
+				<p>{description}</p>
 				<div className="videoFooter_music">
 					<MusicNoteIcon className="videoFooter_icon" />
 					<div className="videoFooterMusic_text">
-						<p>Wow song</p>						
+						<p>{music}</p>						
 					</div>	
 				</div>
 			
